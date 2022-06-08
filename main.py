eae45c382a9725cbaafd03377efada399480dcbf
Eugene-Fed <eugene.a.fed@gmail.com>
2022-06-08 21:24:14 +0300

    Дополнил README.md. Отформатировал вывод нужных столбцов в таблице ad_stat.csv

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 import pandas as pd
 import numpy as np
 
-minus_words = pd.read_csv('minus_words.csv', sep=',')
+minus_words = pd.read_csv('minus_words.csv', sep=',')  # столбцы: minus_words, count
 ad_stat = pd.read_csv('ad_stat.csv', sep=';')
+# столбцы SKU, Название товара, Поисковая фраза, *Запрос пользователя*, *Показы*, *?Клики, CTR (%), Расход (руб., с НДС)
 
 print('Таблица минус-слов')
 print(minus_words.head(5))
 print('\n\nТаблица со статистикой')
-print(ad_stat.head(5))
-input("Press 'Enter'")
+print(ad_stat.head(5)[['Запрос пользователя', 'Показы']])
+# input("Press 'Enter'")
